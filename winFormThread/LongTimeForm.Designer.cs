236368508fbf3f34fe6dcbfb7bd59a13da70ba0f
aifang <aifanghang@163.com>
2015-03-02 13:13:56 +0800

    ok

---
+++ winFormThread/LongTimeForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace winFormThread
+{
+    partial class LongTimeForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLongTime = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLongTime
+            // 
+            this.btnLongTime.Location = new System.Drawing.Point(248, 208);
+            this.btnLongTime.Name = "btnLongTime";
+            this.btnLongTime.Size = new System.Drawing.Size(213, 51);
+            this.btnLongTime.TabIndex = 0;
+            this.btnLongTime.Text = "耗时操作";
+            this.btnLongTime.UseVisualStyleBackColor = true;
+            this.btnLongTime.Click += new System.EventHandler(this.btnLongTime_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(259, 150);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(41, 12);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "label1";
+            // 
+            // LongTimeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 389);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnLongTime);
+            this.Name = "LongTimeForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLongTime;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ winFormThread/LongTimeForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace winFormThread
+{
+    public partial class LongTimeForm : Form
+    {
+        public LongTimeForm()
+        {
+            InitializeComponent();
+            Debug.Listeners.Add(new ConsoleTraceListener());
+        }
+        private string QueryDataBase(IFeatureClass abc)
+        {
+
+            Thread.Sleep(5 * 1000);
+            return "yuyijq";
+        }
+        private string QueryDataBase()
+        {
+            Thread.Sleep(5 * 1000);
+            return "yuyijq";
+        }
+
+        private void SetText(string ret)
+        {
+            this.lblResult.Text = ret;
+        }
+
+        private void btnLongTime_Click(object sender, EventArgs e)
+        {
+            
+            //IFeatureClass fc=null;
+            //string txt="";
+            //Func<IFeatureClass, string> ac = () => QueryDataBase();
+            //ac.BeginInvoke
+            Func<string> func = () => QueryDataBase();
+            //从数据库里获取结果后会更新lblResult.Text属性
+            func.BeginInvoke((result) =>
+            {
+                string ret = func.EndInvoke(result);
+                //注意这里调用Control.BeginInvoke()
+                Action<string> abc = new Action<string>(SetText);
+                this.BeginInvoke(abc, ret);
+            }, null);
+
+            //IAsyncResult ar = this.BeginInvoke(updateTxt, n);
+            ////这里就是你要的返回数据
+            //string result = this.EndInvoke(ar).ToString();
+
+            /*
+             * 当异步操作完成后，上面代码中用lambda表达式表示的一个回调方法就会执行，
+             * 在这里调用EndInvoke获取耗时操作的结果。在这里想想为什么用lambda，
+             * 如果不用lambda也不用匿名方法（不管你用啥，实际上就是形成一个闭包）你要怎么做？
+             */
+        }
+
+        //既然这个WndProc是Win32中处理消息的方法的.Net版，那么我们应该在这里可以监视到所有用户操作的“消息”
+        //protected override void WndProc(ref Message m)
+        //{
+        //    Debug.WriteLine(m.Msg.ToString());
+        //    base.WndProc(ref m);
+        //}
+    }
+}
